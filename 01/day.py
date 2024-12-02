686a0be8fba1c4cba208142eb97877b25642eee4
Alexander Jahn <jahn.alexander@gmail.com>
2024-12-02 09:58:22 +0100

    day 1 part 1

---
+++ 01/day.py
@@ -0,0 +1,33 @@
+import itertools
+import os
+import re
+from collections import defaultdict
+
+this_folder = "\\".join(__file__.split("\\")[:-1])
+
+
+def main(filename):
+    input_data = open(f"{this_folder}/{filename}", "r").read().split("\n")
+    input_data_lists = [list(int(x) for x in line.split()) for line in input_data]
+    col1, col2 = zip(*input_data_lists)
+
+    # Part 1
+    sorted_col1 = sorted(col1)
+    sorted_col2 = sorted(col2)
+
+    _sum = 0
+    for val1, val2 in zip(sorted_col1, sorted_col2):
+        _sum += abs(val1 - val2)
+    result1 = _sum
+    print(f"Part 1 {filename}: ", result1)
+
+    # Part 2
+    result2 = 24
+    print(f"Part 2 {filename}: ", result2)
+    return result1, result2
+
+
+if __name__ == "__main__":
+    assert main("input_example.txt") == (11, 24)
+    assert main("input.txt") == (1590491, 24)
+    # main("input.txt")
