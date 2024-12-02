82c5629635aa687ffc26a4e469bd12f79be9d6c6
Alexander Jahn <jahn.alexander@gmail.com>
2024-12-02 09:58:14 +0100

    initial commit

---
+++ create_todays_folders.ps1
@@ -0,0 +1,21 @@
+# if the first argument is a number, use that as the day number, with leading 0
+# otherwise, use today's date
+if ($args[0] -match "^\d+$") {
+    $todayAsNumNoLeadingZero = $args[0].ToString()
+}
+else {
+    $todayAsNumNoLeadingZero = (Get-Date).ToString("dd").Trim('0')
+}
+$todayAsPaddedNum = $todayAsNumNoLeadingZero.PadLeft(2, '0')
+
+# copy template to new folder
+Copy-Item -r template "$todayAsPaddedNum"
+
+# fetch input from https://adventofcode.com/2023/day/9/input
+code "$todayAsPaddedNum/input2.txt"
+code "$todayAsPaddedNum/day.py"
+code "$todayAsPaddedNum/input.txt"
+
+# open chrome browser
+Start-Process chrome "https://adventofcode.com/2024/day/$todayAsNumNoLeadingZero"
+Start-Process chrome "https://adventofcode.com/2024/day/$todayAsNumNoLeadingZero/input"
---
+++ template/day.py
@@ -0,0 +1,25 @@
+import itertools
+import os
+import re
+from collections import defaultdict
+
+this_folder = "\\".join(__file__.split("\\")[:-1])
+
+
+def main(filename):
+    input_data = open(f"{this_folder}/{filename}", "r").read().split("\n")
+    input_data_mat = [list(line) for line in input_data]
+
+    # Part 1
+    result1 = 42
+    print(f"Part 1 {filename}: ", result1)
+
+    # Part 2
+    result2 = 24
+    print(f"Part 2 {filename}: ", result2)
+    return result1, result2
+
+
+if __name__ == "__main__":
+    assert main("input.txt") == (42, 24)
+    # assert main("input2.txt") == (42, 24)
