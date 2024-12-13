e0a94d8928a8125cef1da153e2fd3e238f33ff7e
Alexander Jahn <jahn.alexander@gmail.com>
2024-12-13 23:02:09 +0100

    day 13 part 2

--- 13/day.py
+++ 13/day.py
@@ -1,8 +1,6 @@
-import itertools
 import math
-import os
 import re
-from collections import defaultdict
+
 
 this_folder = "\\".join(__file__.split("\\")[:-1])
 
@@ -17,6 +15,24 @@ B_COST = 1
 MAX_PUSHES = 100
 
 
+def solver(a_button, b_button, prize_loc):
+    a = a_button[0]
+    b = a_button[1]
+    c = b_button[0]
+    d = b_button[1]
+    p1 = prize_loc[0]
+    p2 = prize_loc[1]
+
+    determinant = a * d - b * c
+    s1 = (d * p1 - c * p2) / determinant
+    s2 = (-b * p1 + a * p2) / determinant
+    solution = [s1, s2]
+    all_vals_are_int = all([val == int(val) for val in solution])
+    if all_vals_are_int:
+        return A_COST * s1 + B_COST * s2, solution
+    return math.inf, solution
+
+
 def main(filename):
     device_infos = open(f"{this_folder}/{filename}", "r").read().split("\n\n")
     device_ab_prizes = []
@@ -25,30 +41,26 @@ def main(filename):
     # Part 1
     result1 = 0
     for button_a, button_b, prize_loc in device_ab_prizes:
-        lowest_option = math.inf
-        for a in range(1, MAX_PUSHES + 1):
-            for b in range(1, MAX_PUSHES + 1):
-                final_loc = (
-                    a * button_a[0] + b * button_b[0],
-                    a * button_a[1] + b * button_b[1],
-                )
-                if final_loc == prize_loc:
-                    current_cost = a * A_COST + b * B_COST
-                    if current_cost < lowest_option:
-                        lowest_option = current_cost
-        if lowest_option != math.inf:
-            result1 += lowest_option
-    print(f"Part 1 {filename}: ", result1)
+        lowest_solution, _ = solver(button_a, button_b, prize_loc)
+        if lowest_solution != math.inf:
+            result1 += lowest_solution
+    print(f"Part 1 {filename}: ", int(result1))
 
     # Part 2
-    result2 = 24
-    print(f"Part 2 {filename}: ", result2)
+    result2 = 0
+    PRICE_OFFSET = 10000000000000
+    for button_a, button_b, prize_loc in device_ab_prizes:
+        prize_loc = (prize_loc[0] + PRICE_OFFSET, prize_loc[1] + PRICE_OFFSET)
+        lowest_solution, _ = solver(button_a, button_b, prize_loc)
+        if lowest_solution != math.inf:
+            result2 += lowest_solution
+    print(f"Part 2 {filename}: ", int(result2))
     return result1, result2
 
 
 if __name__ == "__main__":
     try:
-        assert main("input_example.txt") == (480, 24)
-        assert main("input.txt") == (26299, 24)
+        assert main("input_example.txt") == (480, 875318608908)
+        assert main("input.txt") == (26299, 107824497933339)
     except AssertionError:
         print("❌ wrong")
