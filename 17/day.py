f22fc95cff1570b552eb5d00194541b9fcedb3ed
Alexander Jahn <jahn.alexander@gmail.com>
2024-12-19 07:30:21 +0100

    day 17 part 2 wip

--- 17/day.py
+++ 17/day.py
@@ -30,38 +30,84 @@ def literal_to_combo(literal, reg_A, reg_B, reg_C):
     return None
 
 
+def reverse_combo(literal, value, reg_A, reg_B, reg_C):
+    if literal == 4:
+        reg_A += value
+    if literal == 5:
+        reg_B += value
+    if literal == 6:
+        reg_C += value
+    return reg_A, reg_B, reg_C
+
+
+def reverse_one_step(program, instruction_pointer, reg_A, reg_B, reg_C, output):
+    opcode = program[instruction_pointer]
+    instruction = opcode_to_instruction[opcode]
+    literal_operand = program[instruction_pointer + 1]
+    combo_operand = literal_to_combo(literal_operand, reg_A, reg_B, reg_C)
+
+    if instruction == "adv":
+        reg_A = reg_A * 2**combo_operand
+    elif instruction == "bxl":
+        reg_B = reg_B ^ literal_operand
+    elif instruction == "bst":
+        reg_B = combo_operand % 8
+    elif instruction == "jnz":
+        if reg_A != 0:
+            instruction_pointer = literal_operand
+            return reg_A, reg_B, reg_C, instruction_pointer
+    elif instruction == "bxc":
+        reg_B = reg_B ^ reg_C
+    elif instruction == "out":
+        last_output = output.pop()
+        reg_A, reg_B, reg_C = reverse_combo(
+            literal_operand, last_output, reg_A, reg_B, reg_C
+        )
+    elif instruction == "bdv":
+        reg_B = int(reg_A / 2**combo_operand)
+    elif instruction == "cdv":
+        reg_C = int(reg_A / 2**combo_operand)
+    return reg_A, reg_B, reg_C, instruction_pointer - 2
+
+
+def run_one_step(program, instruction_pointer, reg_A, reg_B, reg_C, output):
+    opcode = program[instruction_pointer]
+    instruction = opcode_to_instruction[opcode]
+    literal_operand = program[instruction_pointer + 1]
+    combo_operand = literal_to_combo(literal_operand, reg_A, reg_B, reg_C)
+
+    if instruction == "adv":
+        reg_A = int(reg_A / 2**combo_operand)
+    elif instruction == "bxl":
+        reg_B = reg_B ^ literal_operand
+    elif instruction == "bst":
+        reg_B = combo_operand % 8
+    elif instruction == "jnz":
+        if reg_A != 0:
+            instruction_pointer = literal_operand
+            return reg_A, reg_B, reg_C, instruction_pointer
+    elif instruction == "bxc":
+        reg_B = reg_B ^ reg_C
+    elif instruction == "out":
+        to_out = combo_operand % 8
+        output.append(to_out)
+    elif instruction == "bdv":
+        reg_B = int(reg_A / 2**combo_operand)
+    elif instruction == "cdv":
+        reg_C = int(reg_A / 2**combo_operand)
+    return reg_A, reg_B, reg_C, instruction_pointer + 2
+
+
 def run_program(program, reg_A, reg_B, reg_C) -> list[int]:
     instruction_pointer = 0
     output = []
     while True:
         if instruction_pointer >= len(program):
             break
-        opcode = program[instruction_pointer]
-        instruction = opcode_to_instruction[opcode]
-        literal_operand = program[instruction_pointer + 1]
-        combo_operand = literal_to_combo(literal_operand, reg_A, reg_B, reg_C)
+        reg_A, reg_B, reg_C, instruction_pointer = run_one_step(
+            program, instruction_pointer, reg_A, reg_B, reg_C, output
+        )
 
-        if instruction == "adv":
-            reg_A = int(reg_A / 2**combo_operand)
-        elif instruction == "bxl":
-            reg_B = reg_B ^ literal_operand
-        elif instruction == "bst":
-            reg_B = combo_operand % 8
-        elif instruction == "jnz":
-            if reg_A != 0:
-                instruction_pointer = literal_operand
-                continue
-        elif instruction == "bxc":
-            reg_B = reg_B ^ reg_C
-        elif instruction == "out":
-            to_out = combo_operand % 8
-            output.append(to_out)
-        elif instruction == "bdv":
-            reg_B = int(reg_A / 2**combo_operand)
-        elif instruction == "cdv":
-            reg_C = int(reg_A / 2**combo_operand)
-
-        instruction_pointer += 2
     return output, reg_A, reg_B, reg_C
 
 
@@ -79,19 +125,21 @@ def part1(filename, return_all=False):
 def part2(filename):
     print(f"Starting part 2 {filename}")
     input_data = open(f"{this_folder}/{filename}", "r").read()
-    reg_A, reg_B, reg_C, *program = map(int, re.findall(r"\d+", input_data))
-    reg_A_to_test = 0
-    len_shown = 0
+    _, _, _, *program = map(int, re.findall(r"\d+", input_data))
+
+    instruction_pointer = len(program) - 4
+    reg_A, reg_B, reg_C = 0, 0, 0
+    output = program.copy()
+    output.pop()
     while True:
-        outputs, _, _, _ = run_program(program, reg_A_to_test, reg_B, reg_C)
-        if outputs is not None:
-            if len(outputs) > len_shown:
-                len_shown = len(outputs)
-                print(reg_A_to_test, program, outputs)
-            if outputs == program:
-                break
-        reg_A_to_test += 1
-    result2 = reg_A_to_test
+        reg_A, reg_B, reg_C, instruction_pointer = reverse_one_step(
+            program, instruction_pointer, reg_A, reg_B, reg_C, output=output
+        )
+        if instruction_pointer < 0 and len(output) == 0:
+            break
+        if instruction_pointer < 0:
+            instruction_pointer = len(program) - 4
+    result2 = reg_A
     print(f"Part 2 {filename}: ", result2)
     return result2
 
